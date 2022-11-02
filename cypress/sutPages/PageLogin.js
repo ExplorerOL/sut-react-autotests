5233c0f7c2f99df1a0523f491efdb56283058f59
ExplorerOL <dvorobev@web-bee.ru>
2022-11-02 17:54:45 +0300

    добавил комментарии к классу PageLogin

--- cypress/sutPages/PageLogin.js
+++ cypress/sutPages/PageLogin.js
@@ -1,9 +1,7 @@
 import Header from "./Header";
 import PageWorkHours from "./PageWorkHours";
 
-//const stand_url = require("../fixtures/stand_url.json"),
-
-    //локаторы
+//локаторы
 const LBL_LOGIN_USING_ACCOUNT = "//p[.='Введите свои учётные данные']",
     INPUT_LOGIN_LABEL = "#mui-1-label",
     INPUT_PASSWORD_LABEL = "#mui-2-label",
@@ -21,60 +19,73 @@ export default class PageLogin {
         this.pageWorkHours = new PageWorkHours;
     }
 
-    //элементы страницы
+//элементы страницы
+    //текст приглашения о вводе учетных данных
     getLblLoginUsingAccount() {
         return cy.xpath(LBL_LOGIN_USING_ACCOUNT);
     }
+    //текст-заглушка в поле логин
     getInputLoginLabel() {
         return cy.get(INPUT_LOGIN_LABEL);
     }
+    //текст-заглушка в поле пароль
     getInputPasswordLabel() {
         return cy.get(INPUT_PASSWORD_LABEL);
     }
+    //поле пароль
     getInputLogin() {
         return cy.get(INPUT_LOGIN);
     }
+    //поле логин
     getInputPassword() {
         return cy.get(INPUT_PASSWORD);
     }
+    //кнопка логин
     getBtnLogin() {
         return cy.get(BTN_LOGIN);
     }
+    //надпись логин обязателен
     getLblLoginIsRequired(){
         return cy.get(LBL_LOGIN_REQUIRED);
     }
+    //надпись пароль обязателен
     getLblPasswordIsRequired(){
         return cy.get(LBL_PASSWORD_REQUIRED);
     }
+    //надпись логин или пароль неверные
     getLblWrongLoginOrPassword(){
         return cy.contains(STYLE_LBL_WRONG_LOGIN_OR_PASSWORD, LBL_WRONG_LOGIN_OR_PASSWORD);
     }
 
-    //действия на странице
+//действия на странице
+    //открыть страницу и проверить наличие ее элементов
     doNavigate() {
         cy.visit("/login");
         this.checkPageElems();
         return this;
     }
+    //нажать кнопку Логин
     doCLickBtnLogin() {
         this.getBtnLogin().click();
         return this;
     }
+    //ввести тест в поле логин
     doTypeInLogin(text) {
         this.getInputLogin().type(text);
         return this;
     }
+    //ввести тест в поле пароль
     doTypeInPassword(text) {
         this.getInputPassword().type(text);
         return this;
     }
+    //процедура логина через UI
     doLogin(userData) {
         this.doNavigate().doTypeInLogin(userData.username).doTypeInPassword(userData.password).doCLickBtnLogin();
         return this.pageWorkHours;
     }
+    //процедура логина через API
     doApiLogin(userData) {
-        // cy.intercept('/api/login/*').as('apiLogin');
-        //         cy.wait('@apiLogin');
         cy.request({
             method: 'POST',
             url: '/api/login',
@@ -91,12 +102,10 @@ export default class PageLogin {
             return this.pageWorkHours;
         });
         
-
-
-
     }
 
-    //функции проверки страницы
+//функции проверки страницы
+    //проверка наличия элементов на странице
     checkPageElems() {
         this.getLblLoginUsingAccount().should('exist').and('be.visible');
         this.getInputLoginLabel().should('contain', 'Логин');
@@ -105,15 +114,18 @@ export default class PageLogin {
         cy.location('protocol').should('eq', 'https:');
         return this;
     }
+    //проверка видимости надписи об обязательности ввода логина
     checkLblLoginIsRequiredVisible() {
         this.getLblLoginIsRequired().should('have.text','Поле обязательно').and('be.visible');
         return this;
     }
+    //проверка видимости надписи об обязательности ввода пароля
     checkLblPasswordIsRequiredVisible() {
         this.getLblPasswordIsRequired().should('have.text','Поле обязательно').and('be.visible');
         return this;
 
     }
+    //проверка видимости надписи о неверном логине или пароле
     checkLblWrongLoginOrPasswordVisible() {
         this.getLblWrongLoginOrPassword().should('be.visible');
         return this;
