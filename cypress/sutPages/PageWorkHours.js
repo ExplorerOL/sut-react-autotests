17bee9d2fc3f5c940c51fcd588bed364aa78fbbf
ExplorerOL <dvorobev@web-bee.ru>
2022-11-02 19:30:59 +0300

    добавил комментарии в класс PageWorkHours

--- cypress/sutPages/PageWorkHours.js
+++ cypress/sutPages/PageWorkHours.js
@@ -1,51 +1,54 @@
-//import header from "./header";
 import Header from './Header.js';
 import Sidebar from './Sidebar.js';
-//import droverAddSickPeriod from "./droverAddSickPeriod";
 
-//const stand_url = require("../fixtures/stand_url.json");
+
 export default class PageWorkHours {
     constructor() {
         this.header = new Header;
         this.sidebar = new Sidebar;
     }
 
-    //элементы страницы
+//элементы страницы
+    //кнопка добавления отсутствий
     getBtnAddLeavePeriods() {
         return cy.xpath("//button[.='Отсутствие']");
     }
+    //кнопка сохранения рабочих часов
     getBtnSaveWorkingHours() {
         return cy.xpath("//button[.='Сохранить']");
     }
+    //название таблицы трудозатрат
     getTitleTableWorkReport() {
         return cy.xpath("//h4[.='Отчет трудозатрат']");
-        }
+    }
+    //название раздела с отсутствиями
     getTitleTableLeavePeriods() {
         return cy.xpath("//h4[.='Отсутствие']");
     }
+    //таблица отсутствий
     getLeavePeriodsList() {
         return cy.get(".ag-center-cols-container");
     }
-
+    //пункт Больничный отпуск в выпадающем списке
     getDropItemSickPeriod() {
         return cy.xpath("//p[.='Больничный']");
     }
     
-    //действия на странице
+//действия на странице
+    //перейти на страницу
     doNavigate() {
         cy.visit("/");
         this.header.getBtnAuthUser().should('exist');
         return this;
     }
+    //открыть дровер добавления больничного
     doOpenDroverAddSickPeriod() {
         this.getBtnAddLeavePeriods().click();
         this.getDropItemSickPeriod().click();
         return droverAddSickPeriod;
     }
-
+    //проверка общих для всех пользователей элементов страницы
     checkCommonElems(userData) {
-        //проверка наличия общих разделов на странице Трудозатраты
-        console.log("3" + userData.username);
         this.header.getBtnAuthUser().contains(userData.username);
         this.sidebar.getSidebar().should('contain', 'Главная');
         this.sidebar.getItemWorkingHours().should('exist')
@@ -57,6 +60,7 @@ export default class PageWorkHours {
         this.getTitleTableLeavePeriods().should('exist');
         return this;        
     }
+    //проверка элементов администратора
     checkAdminElems(userData) {
         this.checkCommonElems(userData);
         this.sidebar.getSidebarListItems().should('have.length', 15);
@@ -69,6 +73,7 @@ export default class PageWorkHours {
         this.sidebar.getItemAProjects().should('exist');
         this.sidebar.getItemASettings().should('exist');
     }
+    //проверка элементов тех ассистента
     checkAssistElems(userData) {
         console.log("2" + userData);
         this.checkCommonElems(userData);
@@ -76,6 +81,7 @@ export default class PageWorkHours {
         this.sidebar.getItemAnalytics().should('exist');
         this.sidebar.getSidebar().should('contain', 'Администрирование');
     }
+    //проверка элементов лида
     checkLeadElems(userData) {
         this.checkCommonElems(userData);
         this.sidebar.getSidebarListItems().should('have.length', 8);
@@ -84,15 +90,13 @@ export default class PageWorkHours {
         this.sidebar.getSidebar().should('not.contain', 'Администрирование');
         
     }
+    //проверка элементов пользователя
     checkUserElems(userData) {
         this.checkCommonElems(userData);
         this.sidebar.getSidebarListItems().should('have.length', 6);
         this.sidebar.getItemAnalytics().should('not.exist');
         this.sidebar.getItemSummaryResPlan().should('not.exist');
         this.sidebar.getSidebar().should('not.contain', 'Администрирование');
-        
     }
-
 }
 
-//module.exports = new PageWorkHours();
