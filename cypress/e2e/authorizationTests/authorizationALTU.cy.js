e5e670746210bdf3793023da527fa069492a4faf
ExplorerOL <dvorobev@web-bee.ru>
2022-11-09 07:52:30 +0300

    переименовал классы с тест-комплектами

--- cypress/e2e/authorizationTests/authorizationALTU.cy.js
+++ cypress/e2e/authorizationTests/authorizationALTU.cy.js
@@ -1,122 +1,114 @@
-import TestSuiteAuthotization from "../../sutTestSuites/TestSuiteAuthorization.js"
+import TestSuiteAuthotization from "../../sutTestSuites/suiteLogin.js";
 import * as API from "../../support/API/apiFunctions";
 
 //файл с набором валидных учетных записей
 const creds_from_file = require("../../fixtures/userCreds.json");
 
-describe('Тесты авторизации пользователей', () => {
-
+describe("Тесты авторизации пользователей", () => {
     beforeEach(() => {
-        localStorage.setItem('sut/onboardingStatus', '{"LaborCostsOnboardingFinished":true}');
+        localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
     });
-    
-    let testAuth = new TestSuiteAuthotization;
+
+    let testAuth = new TestSuiteAuthotization();
     let userData;
 
     //-------------- Admin ---------------
-    it('Вход в систему Admin', () => {
+    it("Вход в систему Admin", () => {
         userData = creds_from_file.admin;
         //логин через UI
         //console.log(userData);
         testAuth.loginUser(userData);
     });
 
-    it('Выход из системы Admin', () => {
+    it("Выход из системы Admin", () => {
         userData = creds_from_file.admin;
         //логин через API
         API.doLogin(userData);
         testAuth.pageWorkHours.doNavigate();
 
         //выход из системы через UI
-        testAuth.logoutUser()
-            .checkPageElems();
+        testAuth.logoutUser().checkPageElems();
     });
 
     //-------------- Tech Assist ---------------
-    it('Вход в систему Tech assist', () => {
+    it("Вход в систему Tech assist", () => {
         userData = creds_from_file.tech_assist;
         //логин через UI
         testAuth.loginUser(userData);
     });
 
-    it('Выход из системы Tech assist', () => {
+    it("Выход из системы Tech assist", () => {
         //логин через API
         API.doLogin(userData);
         testAuth.pageWorkHours.doNavigate();
 
         //выход из системы через UI
-        testAuth.logoutUser()
-            .checkPageElems();
+        testAuth.logoutUser().checkPageElems();
     });
 
     //-------------- Lead ---------------
-    it('Вход в систему Lead', () => {
+    it("Вход в систему Lead", () => {
         userData = creds_from_file.lead;
         //логин через UI
         testAuth.loginUser(userData);
     });
 
-    it('Выход из системы Tech assist', () => {
+    it("Выход из системы Tech assist", () => {
         //логин через API
         API.doLogin(userData);
         testAuth.pageWorkHours.doNavigate();
 
         //выход из системы через UI
-        testAuth.logoutUser()
-            .checkPageElems();
+        testAuth.logoutUser().checkPageElems();
     });
 
-
-     //-------------- User ---------------
-    it('Вход в систему User', () => {
+    //-------------- User ---------------
+    it("Вход в систему User", () => {
         userData = creds_from_file.user;
         //логин через UI
         console.log(userData);
         testAuth.loginUser(userData);
     });
 
-    it('Выход из системы User', () => {
+    it("Выход из системы User", () => {
         userData = creds_from_file.user;
         //логин через API
         API.doLogin(userData);
         testAuth.pageWorkHours.doNavigate();
 
         //выход из системы через UI
-        testAuth.logoutUser()
-            .checkPageElems();
+        testAuth.logoutUser().checkPageElems();
     });
-
 });
- 
-describe('Негативные тесты авторизации', () => {
 
+describe("Негативные тесты авторизации", () => {
     beforeEach(() => {
-        localStorage.setItem('sut/onboardingStatus', '{"LaborCostsOnboardingFinished":true}');
+        localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
     });
-    
-    let testAuth = new TestSuiteAuthotization;
+
+    let testAuth = new TestSuiteAuthotization();
     let userData;
 
-     //-------------- Негативные проверки ---------------
-    it('Вход с пустым логином', () => {
+    //-------------- Негативные проверки ---------------
+    it("Вход с пустым логином", () => {
         userData = creds_from_file.user;
         //логин через UI
         console.log(userData);
         testAuth.loginWithoutLogin(userData);
     });
-    it('Вход с пустым паролем', () => {
+    it("Вход с пустым паролем", () => {
         userData = creds_from_file.user;
         //логин через UI
         console.log(userData);
         testAuth.loginWithoutPassword(userData);
     });
-    it('Вход с неправильным логином', () => {
+    it("Вход с неправильным логином", () => {
         userData = creds_from_file.wrong_login_user;
         //логин через UI
         console.log(userData);
         testAuth.loginWithWrongCreds(userData);
     });
-    it('Вход с неправильным паролем', () => {
+    it("Вход с неправильным паролем", () => {
         userData = creds_from_file.wrong_password_user;
         //логин через UI
         console.log(userData);
