37a17e7ad86d30bc999269bd0ad7735a13a94228
ExplorerOL <dvorobev@web-bee.ru>
2022-11-14 08:48:08 +0300

    сделал цикл тестов по пользователям с возможностью выбирать пользователей через переменную окуржения

--- cypress/e2e/sut-smokes/smoke.cy.js
+++ cypress/e2e/sut-smokes/smoke.cy.js
@@ -7,102 +7,106 @@ import * as helpers from "../../support/helpers.js";
 const creds = require("../../fixtures/validUserCreds");
 const leavePeriodTypes = require("../../fixtures/leavePeriodTypes.json");
 
-let userFromEnv = Cypress.env("userForTest");
-let userCreds = creds[userFromEnv];
+for (let nameOfUserObj in creds) {
+    let userCreds = creds[nameOfUserObj];
+    if (Cypress.env("userForTest") != "ALL" && nameOfUserObj != Cypress.env("userForTest")) {
+        continue;
+    }
 
-describe("Смоук тест " + userFromEnv, () => {
-    beforeEach(() => {
-        localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
-    });
-
-    describe("Авторизация " + userFromEnv, () => {
-        let suitLogin = new SuitLogin();
-
-        it("Вход в систему " + userFromEnv, function () {
-            //логин через UI
-            suitLogin.doLoginUserAndCheckVisibleElems(userCreds).doNavigate();
-        });
-
-        it("Выход из системы " + userFromEnv, () => {
-            //логин через API
-            API.doLogin(userCreds).then((POSTResponseBody) => {
-                API.saveUserInfoAndSetCookies(POSTResponseBody);
-            });
-            suitLogin.pageLaborReports.doNavigate();
-
-            //выход из системы через UI
-            suitLogin.pageLaborReports.header.doLogout();
-        });
-    });
-
-    describe("3.1.2 Отсутствия " + userFromEnv, () => {
-        //let userCreds = creds.admin;
-        let suitLaborReports = new SuiteLaborReports();
-        let token;
-        let startDate = helpers.calculateLeavePeriodStartDateYYYYMMDD();
-        let endDate = helpers.calculateLeavePeriodEndDateYYYYMMDD();
-
-        before(() => {
-            localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
-            // API.doLoginAndSaveUserInfoAndToken(userCreds).then(() => {
-            //     token = Cypress.env("userInfoFromPOST").token;
-            // });
-            API.doLogin(userCreds)
-                .then((POSTResponseBody) => {
-                    API.saveUserInfoAndSetCookies(POSTResponseBody);
-                })
-                .then(() => {
-                    token = Cypress.env("userAuthInfoByAPI").token;
-                });
-        });
-
+    describe("Смоук тест " + nameOfUserObj, () => {
         beforeEach(() => {
-            cy.setCookie("auth_token", token);
-            API.deleteAllLeavePeriods(Cypress.env("userAuthInfoByAPI"));
+            localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
         });
 
-        it("3.1.2.1. Добавление больничного со страницы трудозатрат " + userFromEnv, () => {
-            suitLaborReports.addLeavePeriod(leavePeriodTypes.sickPeriodType);
+        describe("Авторизация " + nameOfUserObj, () => {
+            let suitLogin = new SuitLogin();
+
+            it("Вход в систему " + nameOfUserObj, function () {
+                //логин через UI
+                suitLogin.doLoginUserAndCheckVisibleElems(userCreds).doNavigate();
+            });
+
+            it("Выход из системы " + nameOfUserObj, () => {
+                //логин через API
+                API.doLogin(userCreds).then((POSTResponseBody) => {
+                    API.saveUserInfoAndSetCookies(POSTResponseBody);
+                });
+                suitLogin.pageLaborReports.doNavigate();
+
+                //выход из системы через UI
+                suitLogin.pageLaborReports.header.doLogout();
+            });
         });
 
-        it("3.1.2.1. Добавление ежегодного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            suitLaborReports.addLeavePeriod(leavePeriodTypes.vacationPeriodType);
-        });
+        describe("3.1.2 Отсутствия " + nameOfUserObj, () => {
+            //let userCreds = creds.admin;
+            let suitLaborReports = new SuiteLaborReports();
+            let token;
+            let startDate = helpers.calculateLeavePeriodStartDateYYYYMMDD();
+            let endDate = helpers.calculateLeavePeriodEndDateYYYYMMDD();
 
-        it("3.1.2.1. Добавление административного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            suitLaborReports.addLeavePeriod(leavePeriodTypes.administrativePeriodType);
-        });
-        it("3.1.2.1. Добавление декретного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            suitLaborReports.addLeavePeriod(leavePeriodTypes.maternityPeriodType);
-        });
+            before(() => {
+                localStorage.setItem("sut/onboardingStatus", '{"LaborCostsOnboardingFinished":true}');
+                // API.doLoginAndSaveUserInfoAndToken(userCreds).then(() => {
+                //     token = Cypress.env("userInfoFromPOST").token;
+                // });
+                API.doLogin(userCreds)
+                    .then((POSTResponseBody) => {
+                        API.saveUserInfoAndSetCookies(POSTResponseBody);
+                    })
+                    .then(() => {
+                        token = Cypress.env("userAuthInfoByAPI").token;
+                    });
+            });
 
-        it("3.1.2.1. Удаление больничного со страницы трудозатрат " + userFromEnv, () => {
-            let periodType = leavePeriodTypes.sickPeriodType;
-            API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
-            suitLaborReports.pageLaborReports.doNavigate();
+            beforeEach(() => {
+                cy.setCookie("auth_token", token);
+                API.deleteAllLeavePeriods(Cypress.env("userAuthInfoByAPI"));
+            });
 
-            suitLaborReports.deleteMostRecentLeavePeriod(periodType);
-        });
-        it("3.1.2.1. Удаление ежегодного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            let periodType = leavePeriodTypes.vacationPeriodType;
-            API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
-            suitLaborReports.pageLaborReports.doNavigate();
+            it("3.1.2.1. Добавление больничного со страницы трудозатрат " + nameOfUserObj, () => {
+                suitLaborReports.addLeavePeriod(leavePeriodTypes.sickPeriodType);
+            });
 
-            suitLaborReports.deleteMostRecentLeavePeriod(periodType);
-        });
-        it("3.1.2.1. Удаление административного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            let periodType = leavePeriodTypes.administrativePeriodType;
-            API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
-            suitLaborReports.pageLaborReports.doNavigate();
+            it("3.1.2.1. Добавление ежегодного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                suitLaborReports.addLeavePeriod(leavePeriodTypes.vacationPeriodType);
+            });
 
-            suitLaborReports.deleteMostRecentLeavePeriod(periodType);
-        });
-        it("3.1.2.1. Удаление декретного отпуска со страницы трудозатрат " + userFromEnv, () => {
-            let periodType = leavePeriodTypes.maternityPeriodType;
-            API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
-            suitLaborReports.pageLaborReports.doNavigate();
+            it("3.1.2.1. Добавление административного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                suitLaborReports.addLeavePeriod(leavePeriodTypes.administrativePeriodType);
+            });
+            it("3.1.2.1. Добавление декретного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                suitLaborReports.addLeavePeriod(leavePeriodTypes.maternityPeriodType);
+            });
 
-            suitLaborReports.deleteMostRecentLeavePeriod(periodType);
+            it("3.1.2.1. Удаление больничного со страницы трудозатрат " + nameOfUserObj, () => {
+                let periodType = leavePeriodTypes.sickPeriodType;
+                API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
+                suitLaborReports.pageLaborReports.doNavigate();
+
+                suitLaborReports.deleteMostRecentLeavePeriod(periodType);
+            });
+            it("3.1.2.1. Удаление ежегодного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                let periodType = leavePeriodTypes.vacationPeriodType;
+                API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
+                suitLaborReports.pageLaborReports.doNavigate();
+
+                suitLaborReports.deleteMostRecentLeavePeriod(periodType);
+            });
+            it("3.1.2.1. Удаление административного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                let periodType = leavePeriodTypes.administrativePeriodType;
+                API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
+                suitLaborReports.pageLaborReports.doNavigate();
+
+                suitLaborReports.deleteMostRecentLeavePeriod(periodType);
+            });
+            it("3.1.2.1. Удаление декретного отпуска со страницы трудозатрат " + nameOfUserObj, () => {
+                let periodType = leavePeriodTypes.maternityPeriodType;
+                API.addLeavePeriod(Cypress.env("userAuthInfoByAPI"), periodType, startDate, endDate);
+                suitLaborReports.pageLaborReports.doNavigate();
+
+                suitLaborReports.deleteMostRecentLeavePeriod(periodType);
+            });
         });
     });
-});
+}
